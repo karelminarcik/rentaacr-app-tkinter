7e4700f3aff62a9b4ab98ef8dc7133e9d45b8b03
Karel Minarcik <k.minarcik@seznam.cz>
2024-05-21 21:36:06 +0200

    Created GUI, problem with img in canvas.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from tkinter import *
 import pandas as pd
 
+from PIL import Image, ImageTk
+
 BLACK = "#0F0F0F"
 GREY = "#232D3F"
 BLUE = "#0ef"
 FONT = "Arial"
-
-
-
 
 
 # Loading data from csv files
@@ -17,18 +16,18 @@ df_age_percentage = pd.read_csv("rokyvsprocenta.csv")
 
 # List of military ranks
 hodnost = df_rank_salary["Hodnost"]
+hodnost_dict = df_rank_salary.to_dict()
+print(hodnost_dict)
 hodnost_list = hodnost.to_list()
 hodnost = [i.replace('\xa0', '') for i in hodnost]
 
-# Liat of service age 
-
+# List of service age 
 age = df_age_percentage["Roky"]
 age = age.to_list()
-print(age)
 
-
-
-
+def count():
+    selected_rank = value_rank.get()
+    selected_age = value_age.get()   
 
 
 window = Tk()
@@ -39,7 +38,7 @@ window.resizable(False, False)
 window.iconbitmap("soldier_icon.ico")
 
 header = Label(text="Renta AČR Kalkulačka",pady=10, bg=BLACK, fg=BLUE, font=(FONT, 24, "bold"))
-header.grid(row=0, column=1)
+header.grid(row=0, column=1, columnspan=3)
 
 rank_label = Label(text="Hodnost", bg=BLACK, fg=BLUE, font=(FONT, 16) )
 rank_label.grid(row=1, column=0)
@@ -51,14 +50,36 @@ extra_money_label = Label(text="Výkonnostní příspěvek", bg=BLACK, fg=BLUE, 
 extra_money_label.grid(row=1, column=2)
 
 # Create a Dropdown menu widget with the list of military ranks
-rank_dropdown_menu = OptionMenu( window , StringVar(value="Vojín") , *hodnost) 
-rank_dropdown_menu.configure(bg=BLUE, fg=GREY, width=12, activebackground=BLACK, activeforeground=BLUE, font=(FONT, 16))
+value_rank =StringVar(window)
+# Set the default value of the variable 
+value_rank.set("Vojín") 
+rank_dropdown_menu = OptionMenu( window , value_rank , *hodnost) 
+rank_dropdown_menu.configure(bg=BLUE, fg=GREY, width=12, activebackground=BLACK, activeforeground=BLUE, font=(FONT, 16), cursor="hand2")
 rank_dropdown_menu.grid(row=2, column=0)
 
 # Create a Dropdown menu widget with the list of service age
-age_dropdown_menu = OptionMenu( window , StringVar(value="15") , *age) 
-age_dropdown_menu.configure(bg=BLUE, fg=GREY, width=12, activebackground=BLACK, activeforeground=BLUE, font=(FONT, 16))
+value_age =StringVar(window)
+# Set the default value of the variable 
+value_age.set("15") 
+age_dropdown_menu = OptionMenu( window , value_age , *age) 
+age_dropdown_menu.configure(bg=BLUE, fg=GREY, width=12, activebackground=BLACK, activeforeground=BLUE, font=(FONT, 16), cursor="hand2")
 age_dropdown_menu.grid(row=2, column=1)
+
+extra_money_spinbox = Spinbox(window, from_=0, to=20, width=12, font=(FONT, 16), bg=BLUE, fg=BLACK)
+extra_money_spinbox.config(state="normal", cursor="hand2", bd=3, justify="center", wrap=True, )
+extra_money_spinbox.grid(row=2,column=2)
+
+canvas = Canvas(height=200, width=400)
+canvas.grid(row=3, column=0, columnspan=3)
+# Load an image using PIL
+money_image = PhotoImage(file="money.png")
+
+# Add the image to the canvas
+canvas.create_image(100, 100, anchor=NW, image=money_image)
+
+button = Button(text="Spočítat")
+button.grid(row=4, column=1, pady=10)
+button.configure(font=(FONT, 16), cursor="hand2",bg=BLUE, fg=BLACK, command=count)
 
 
 
